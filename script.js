9a7ae4d91d17826aea2d5b5cad5cf3c1b970577e
CEOChif <108474137+CEOChif@users.noreply.github.com>
2023-11-28 13:00:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Yay, asambe siyokudla iIce Cream !!";
+  gif.src =   "https://media0.giphy.com/media/B3MRnrwSvfiXhvscxV/giphy.gif?cid=6c09b9521da0w68s61lesey4itx06v7zxwtrjahad5x02t3p&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=s";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
